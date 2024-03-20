463c6e5269696848e937fa8ca9baa8433e0c6cbf
HasanCetin <hasancetinn8636@gmail.com>
2024-03-20 14:10:00 +0300

    user controller service ve modul kısmı bir takım ilerlemeler katedildi.

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,3 +1,22 @@
-export class UsersController{
-    
+import { Controller, Get, UseGuards, Req, Patch, Param, Body } from '@nestjs/common';
+//import { JwtAuthGuard } from './jwt-auth.guard'; // Kullanıcı kimlik doğrulaması için JWT tabanlı guard
+import { UserService } from './users.service';
+
+@Controller('user')
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+
+
+    //@UseGuards(JwtAuthGuard) 
+    @Get()
+    //bu kısım oturum açan kullanıcının bilgilerini getirmek içindir.
+    async getUser(@Req() request) {
+        const userId = request.user.id;
+        return this.userService.findById(userId);
+    }
+    @Patch(':id/update-username') // PATCH mevcut kaydı kısmen güncellemek için kullanılmaktadır.
+    async updateUsername(@Param('id') userId: string, @Body() body: { username: string }) {
+        const { username } = body;//güncellenecek isim
+        return this.userService.updateUsername(userId, username);
+    }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,3 +1,25 @@
-export class UsersService{
-    
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './users.dto';
+
+@Injectable()
+export class UserService {
+    constructor(@InjectModel(User.name) private readonly userModel: Model<User>) { }
+
+    // Kullanıcıyı ID'ye göre bulma kısmı 
+    //.exec() kısmı sorguları Promise olarak döndürür.
+    //eğer kullanıcı yok ise 'null' değeri döndürmektedir.
+    async findById(userId: string): Promise<User> {
+        return this.userModel.findById(userId).exec();
+    }
+    async updateUsername(userId: string, newUsername: string): Promise<User> {
+        const user = await this.userModel.findById(userId);
+        if (!user) {
+          throw new NotFoundException('Kullanıcı bulunamadı');
+        }
+        user.username = newUsername;
+
+        return user.save();
+      }
 }
