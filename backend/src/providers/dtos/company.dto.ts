651ccb5830f0d72a77de910cebd5378cf3b51185
HasanCetin <hasan_8636@hotmail.com>
2024-03-22 16:06:14 +0300

    users kısmında iyileştirmeler yapıldı. Company.dto ve schema oluşturuldu

---
+++ backend/src/providers/dtos/company.dto.ts
@@ -0,0 +1,9 @@
+export class CompanyDTO {
+    name: string
+    address: string
+    password:string
+    email: string
+    year_founded:Date
+    description:string
+
+}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,34 +1,58 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from './users.dto';
-
+import { User } from './dtos/users.dto';
+//1) Kullanıcı tüm bilgilerini görebilcek ; İsim, e-posta, şifre, 
+//2)Kullanıcı satın aldığı hizmetlere erişebilecek.
+//3)Kullanıcı kişisel bilgilerini güncelleyebilecek
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private readonly userModel: Model<User>) { }
+    constructor(@InjectModel(User.name) private readonly userModel: Model<User>) { }
 
-  // Kullanıcıyı ID'ye göre bulma kısmı 
-  //.exec() kısmı sorguları Promise olarak döndürür.
-  //eğer kullanıcı yok ise 'null' değeri döndürmektedir.
-  async findById(userId: string): Promise<User> {
-    return this.userModel.findById(userId).exec();
-  }
-  async updateUsername(userId: string, newUsername: string): Promise<User> {
-    const user = await this.userModel.findById(userId);
-    if (!user) {
-      throw new NotFoundException('Kullanıcı bulunamadı');
+    // Kullanıcıyı ID'ye göre bulma kısmı 
+    //.exec() kısmı sorguları Promise olarak döndürür.
+    //eğer kullanıcı yok ise 'null' değeri döndürmektedir.
+    async findById(userId: string): Promise<User> {
+        return this.userModel.findById(userId).exec();
     }
-    user.username = newUsername;
+    /*async updateUsername(userId: string, newUsername: string): Promise<User> {
+        const user = await this.userModel.findById(userId);
+        if (!user) {
+            throw new NotFoundException('Kullanıcı bulunamadı');
+        }
+        user.username = newUsername;
 
-    return user.save();
-  }
-  async updatePassword(userId: string, newPassword: string): Promise<User> {
-    const user = await this.userModel.findById(userId);
-    if (!user) {
-      throw new NotFoundException('Lütfen mevcut şifrenizi doğru giriniz.');
+        return user.save();
     }
-    user.password = newPassword;
+    async updatePassword(userId: string, newPassword: string): Promise<User> {
+        const user = await this.userModel.findById(userId);
+        if (!user) {
+            throw new NotFoundException('Lütfen mevcut şifrenizi doğru giriniz.');
+        }
+        user.password = newPassword;
 
-    return user.save();
-  }
+        return user.save();
+    }*/
+    async updateUserField(userId: string, field: 'username' | 'password' | 'email', value: string): Promise<User> {
+        // Kullanıcıyı bul
+        const user = await this.userModel.findById(userId);
+        if (!user) {
+            throw new NotFoundException('Kullanıcı bulunamadı');
+        }
+        // Alanlar ve karşılık gelen özelliklerin bir dizisi
+        const fields = {
+            'username': 'username',
+            'password': 'password',
+            'email': 'email'
+        };
+        // Belirtilen alanın doğru olup olmadığını kontrol et
+        const propertyName = fields[field];
+        if (!propertyName) {
+            throw new BadRequestException('Geçersiz alan');
+        }
+        // Belirtilen alana göre güncelleme yap
+        user[propertyName] = value;
+        
+        return user.save();
+    }
 }
