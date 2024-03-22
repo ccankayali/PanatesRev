93dc0dce08ee9a4c408b25e93338687a2b88a2a3
HasanCetin <hasan_8636@hotmail.com>
2024-03-22 16:09:24 +0300

    users kısmında iyileştirmeler yapıldı. Company.dto ve schema oluşturuldu

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,9 +2,7 @@ import { BadRequestException, Injectable, NotFoundException } from '@nestjs/comm
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './dtos/users.dto';
-//1) Kullanıcı tüm bilgilerini görebilcek ; İsim, e-posta, şifre, 
-//2)Kullanıcı satın aldığı hizmetlere erişebilecek.
-//3)Kullanıcı kişisel bilgilerini güncelleyebilecek
+
 @Injectable()
 export class UserService {
     constructor(@InjectModel(User.name) private readonly userModel: Model<User>) { }
