f235f2606c7ee2e744c8d3fad653054e80b3c666
GorkemKurtkaya <gorkem.kurtkaya@yahoo.com>
2024-03-25 14:56:03 +0300

    ekleme

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServicesModule } from './services/services.module';
+import { ShopModule } from './shop/shop.module';
+import { UsersModule } from './users/users.module';
+import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/library-next-api'),AuthModule,],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/library-next-api'),AuthModule,ServicesModule,ShopModule,UsersModule,PostModule,],
   controllers: [AppController],
   providers: [AppService,],
 })
