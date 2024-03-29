5200a7cfc16cac5be316b68ba1f5792c268aeee7
GorkemKurtkaya <gorkem.kurtkaya@yahoo.com>
2024-03-29 16:57:14 +0300

    backend ve frontend güncellemeleri yapıldı

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -34,6 +34,7 @@ export class UserController {
     }*/
     @Patch(':id/update-field')
     async updataField(@Param('id') userId: string,
+    
         @Body() body: { field: 'name' | 'password' | 'email', value: string }) {
         const { field, value } = body
         if (field !== 'name' && field !== 'password' && field !== 'email') {
---
+++ frontend/src/templatesapp/signup.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import "./signup.css";
+import { useNavigate } from "react-router-dom";
+
+
+export function SignUp() {
+    
+    const { navigate } = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:3000/auth/signup_user/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: e.target.name.value,
+                email: e.target.email,
+                password: e.target.password.value
+            })
+        }).then(response => {
+            navigate('/login')
+            response.json()}).catch(error => console.log(error))
+    }
+
+    return(
+        <div class="container">
+        <h1>SignUp</h1>
+        <form action="" method="post" onSubmit={handleSubmit}>
+            <label for="name">Adınız</label>
+            <input type="text" id="name" name="name" required/>
+
+            {/* <label for="username">Username</label>
+            <input type="text" id="username" name="username" required/> */}
+
+            <label for="email">Email address</label>
+            <input type="email" id="email" name="email" required/>
+
+            <label for="password">Password</label>
+                    <input type="password" id="password" name="password" required />
+
+                    {/* <label for="confirm_password">Confirm Password</label>
+                    <input type="password" id="confirm_password" name="confirm_password" required /> */}
+
+                    <button type="submit">Sign up</button>
+                    <p>Already have an account? <a href="/login">Sign in</a></p>
+                </form>
+            </div>
+    )
+    
+
+}
