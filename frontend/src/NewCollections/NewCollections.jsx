ee0f74e7f598426a416ec196d6a1a21178e5f623
atacemrek <cemrek.ata@gmail.com>
2024-04-16 09:41:31 +0300

    bastan yapilacak bu degisiklik onaylanmayacak

---
+++ frontend/src/NewCollections/NewCollections.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './NewCollections.css';
+
+const collections = () => {
+    return(
+        <div className='newcollections'>
+        </div>
+    )
+};
+export default newcollections;
